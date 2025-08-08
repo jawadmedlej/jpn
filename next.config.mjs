3e91df329c64e186eda1dcab13efe7442159dbfb
jawad <jawad.medlej0101@gmail.com>
2025-08-08 11:07:05 -0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',                // Incoming path
+        destination: '/index.html', // Target path
+        permanent: false,           // 307 (temporary) if false, 308 (permanent) if true
+      },
+    ];
+  },
+};
 
 export default nextConfig;
